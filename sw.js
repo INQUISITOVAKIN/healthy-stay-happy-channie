9a6197b3c1e58cdabb53f3016e7c6e0a088bbc07
INQUISITOVAKIN <322197507+INQUISITOVAKIN@users.noreply.github.com>
2026-09-01 11:56:35 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="happy-channie-v24";
+const CACHE="happy-channie-v25";
 self.addEventListener("install",event=>{self.skipWaiting();});
 self.addEventListener("activate",event=>{
   event.waitUntil(
